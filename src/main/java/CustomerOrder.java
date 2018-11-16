dfbdf2306822d997ca54347262a4b53b344530c6
guilherme.marcal <Guilherme>
2018-11-16 17:45:29 -0200

    NEW

---
+++ src/main/java/CustomerOrder.java
@@ -0,0 +1,78 @@
+
+public class CustomerOrder {
+
+    String companyname, contactname, contacttitle, address, city, state, country;
+    int phone, CEP;
+
+    public String getCompanyname() {
+        return companyname;
+    }
+
+    public void setCompanyname(String companyname) {
+        this.companyname = companyname;
+    }
+
+    public String getContactname() {
+        return contactname;
+    }
+
+    public void setContactname(String contactname) {
+        this.contactname = contactname;
+    }
+
+    public String getContacttitle() {
+        return contacttitle;
+    }
+
+    public void setContacttitle(String contacttitle) {
+        this.contacttitle = contacttitle;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public int getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(int CEP) {
+        this.CEP = CEP;
+    }
+}
